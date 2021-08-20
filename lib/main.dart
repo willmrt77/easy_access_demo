fef89353e4f0fc059f98ec808c46b02c052cc102
willmrt77 <will@bribrotech.com>
2021-08-20 13:59:04 -0400

    Testing scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:easy_access_demo/screens/nfc_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_access_demo/screens/welcome_screen.dart';
 import 'package:easy_access_demo/screens/login_screen.dart';
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'screens/test_scanner.dart';
+import 'screens/nfc_manager.dart';
 
 void main() async {
   Socket sock = await Socket.connect('192.168.0.164', 80);
@@ -31,7 +32,7 @@ class EasyAccessDemo extends StatelessWidget {
         routes: {
           WelcomeScreen.id: (context) => WelcomeScreen(),
           LoginScreen.id: (context) => LoginScreen(),
-          // ChatScreen.id: (context) => ChatScreen(),
+          NfcScanner.id: (context) => NfcScanner(),
           TestScanner.id: (context) => TestScanner(
                 channel: socket,
               ),
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,9 +1,11 @@
 // import 'package:easy_access/screens/scanner.dart';
+import 'package:easy_access_demo/screens/nfc_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:easy_access_demo/screens/login_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:easy_access_demo/components/rounded_button.dart';
+import 'nfc_manager.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -86,6 +88,13 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               color: Colors.blueAccent,
               onPressed: () {
                 Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButton(
+              title: 'TEST NFC',
+              color: Colors.blueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, NfcScanner.id);
               },
             ),
           ],
